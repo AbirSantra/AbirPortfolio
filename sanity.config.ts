863febc25c6507b5218e998e0929c03a0709ba97
Abir Santra <santraabir8@gmail.com>
2023-07-05 20:12:21 +0530

    Created sanity config file

---
+++ sanity.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "sanity";
+import {deskTool} from 'sanity/desk'
+
+const config = defineConfig({
+  name: 'Abir Portfolio',
+  title: 'AbirPortfolio',
+
+  projectId: 'm1lgynxf',
+  dataset: 'production',
+  apiVersion: '2023-07-05',
+  basePath: '/admin',
+
+  plugins: [deskTool()],
+
+})
+
+export default config;
