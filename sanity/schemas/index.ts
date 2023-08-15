2d9af28cabf7624b3edbdcc117ca95fe13cc3503
Abir Santra <santraabir8@gmail.com>
2023-08-15 22:31:24 +0530

    imported schemas

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,8 @@
+import education from "./education-schema";
+import experience from "./experience-schema";
 import project from "./project-schema";
 import tool from "./tool-schema";
 
-const schemas = [project, tool];
+const schemas = [project, tool, experience, education];
 
 export default schemas;
