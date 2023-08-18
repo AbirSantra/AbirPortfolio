42e895749fc218093ceeb4ef607bb6a98fb11ff0
Abir Santra <santraabir8@gmail.com>
2023-08-18 20:39:29 +0530

    added logo

---
+++ components/Logo.tsx
@@ -0,0 +1,10 @@
+const Logo = () => {
+  return (
+    <div className="flex text-xl font-medium -tracking-wider">
+      <p>abir</p>
+      <p className="gradient-text">._</p>
+    </div>
+  );
+};
+
+export default Logo;
