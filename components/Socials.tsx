94d636079fff73050b682e17673a70c8d392758a
Abir Santra <santraabir8@gmail.com>
2023-08-19 19:40:53 +0530

    created socials component

---
+++ components/Socials.tsx
@@ -0,0 +1,48 @@
+import LinkedinIcon from "../public/images/linkedin-icon.svg";
+import GithubIcon from "../public/images/github-icon.svg";
+import TwitterIcon from "../public/images/twitter-icon.svg";
+import InstagramIcon from "../public/images/instagram-icon.svg";
+import HashnodeIcon from "../public/images/hashnode-icon.svg";
+import Link from "next/link";
+import Image from "next/image";
+
+export const Socials = () => {
+  const socialsList = [
+    {
+      name: "Linkedin",
+      icon: LinkedinIcon,
+      link: "https://www.abirsantra.com",
+    },
+    {
+      name: "Github",
+      icon: GithubIcon,
+      link: "https://www.abirsantra.com",
+    },
+    {
+      name: "Hashnode",
+      icon: HashnodeIcon,
+      link: "https://www.abirsantra.com",
+    },
+    {
+      name: "Twitter",
+      icon: TwitterIcon,
+      link: "https://www.abirsantra.com",
+    },
+    {
+      name: "Instagram",
+      icon: InstagramIcon,
+      link: "https://www.abirsantra.com",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-center gap-4 sm:gap-6">
+      {socialsList.map((social, index) => {
+        return (
+          <Link key={index} href={social.link}>
+            <Image src={social.icon} alt={social.name} className="w-8" />
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
