98dcbc7119a4f53fcc8857f7ef22a82e3eca2967
Abir Santra <santraabir8@gmail.com>
2023-08-20 20:47:31 +0530

    increased left margin for right variant

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -9,10 +9,10 @@ export const Tooltip = ({ children, text, direction }: Props) => {
     <div className="group relative inline-block">
       {children}
       <p
-        className={`invisible absolute whitespace-nowrap rounded-md bg-white p-2 text-xs text-gray-950 opacity-0 shadow-sm duration-100 ease-linear group-hover:visible group-hover:opacity-100 dark:bg-gray-900 dark:text-white ${
+        className={`invisible absolute whitespace-nowrap rounded-md bg-white p-2 text-xs text-neutral-950 opacity-0 shadow-sm duration-100 ease-linear group-hover:visible group-hover:opacity-100 dark:bg-neutral-800 dark:text-white ${
           direction === "bottom" && "left-1/2 top-full mt-2 -translate-x-1/2"
         } ${
-          direction === "right" && "left-full top-1/2 ml-4 -translate-y-1/2"
+          direction === "right" && "left-full top-1/2 ml-6 -translate-y-1/2"
         } ${
           direction === "left" && "right-full top-1/2 mr-4 -translate-y-1/2"
         } ${
