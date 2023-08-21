ecbdddb84461912ef40d088a43b80f315c4ea538
Abir Santra <santraabir8@gmail.com>
2023-08-21 21:46:50 +0530

    added Exp section

---
+++ sections/Experience-section.tsx
@@ -0,0 +1,20 @@
+import { ExperienceCard } from "@/components/ExperienceCard";
+import { SectionHeader } from "@/components/SectionHeader";
+import { getAllExperiences } from "@/sanity/sanity-queries";
+
+export const ExperienceSection = async () => {
+  const experienceDocs = await getAllExperiences();
+  return (
+    <div className="flex flex-col gap-8">
+      <SectionHeader
+        title="Experience"
+        subtitle="Developing, one step at a time"
+      />
+      <div className="ml-4 flex flex-col gap-16 border-l-4 border-neutral-200 pl-8 dark:border-neutral-800">
+        {experienceDocs.map((experienceDoc) => (
+          <ExperienceCard data={experienceDoc} key={experienceDoc._id} />
+        ))}
+      </div>
+    </div>
+  );
+};
