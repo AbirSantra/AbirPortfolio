a6ad022520a8a4fc8707b2f666065be645571c58
Abir Santra <santraabir8@gmail.com>
2023-08-23 19:53:22 +0530

    added experience-education section

--- sections/Experience-Education-section.tsx
+++ sections/Experience-Education-section.tsx
@@ -3,9 +3,10 @@ import { EducationSection } from "./Education-section";
 
 export const ExperienceEducationSection = () => {
   return (
-    <div className="section-padding grid grid-cols-1 gap-16 md:grid-cols-2">
+    <div className="section-padding grid grid-cols-1 gap-56 md:grid-cols-2 md:gap-16">
       {/* @ts-expect-error Server Component */}
       <ExperienceSection />
+      {/* @ts-expect-error Server Component */}
       <EducationSection />
     </div>
   );
