73e2fb9fb89708d00f7c9bb47f6492a63f0e47e9
Abir Santra <santraabir8@gmail.com>
2023-08-23 19:53:35 +0530

    created education type

---
+++ types/Education-type.ts
@@ -0,0 +1,10 @@
+export type Education = {
+  _id: string;
+  institution: string;
+  institutionLogo: string;
+  field: string;
+  location: string;
+  startDate: Date;
+  endDate: Date;
+  score: string;
+}
