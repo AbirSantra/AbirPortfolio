0f730607491d065d290c25d94a4b611163dff577
Abir Santra <santraabir8@gmail.com>
2023-08-24 19:50:05 +0530

    added hover animation to toolcard

--- components/ToolCard.tsx
+++ components/ToolCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const ToolCard = ({ data }: { data: Tool }) => {
   return (
-    <div className="flex items-center gap-2 rounded-lg bg-white px-4 py-2 shadow-sm dark:bg-neutral-800">
+    <div className="flex cursor-pointer items-center gap-2 rounded-lg border-[1px] border-transparent bg-white px-4 py-2 shadow-sm duration-200 hover:border-sky-500 dark:bg-neutral-800">
       <Image src={data.icon} alt={data.name} height={16} width={16} />
       <p className="text-sm">{data.name}</p>
     </div>
