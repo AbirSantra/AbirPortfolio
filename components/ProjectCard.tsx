3ab17673dff21f16a322b7d57a35cc0823e73320
Abir Santra <santraabir8@gmail.com>
2023-08-25 19:37:21 +0530

    added image scale on hover

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -5,7 +5,13 @@ import React from "react";
 export const ProjectCard = ({ data }: { data: Project }) => {
   return (
     <div className="group relative flex overflow-hidden rounded-lg md:first:col-span-2 md:first:row-span-2">
-      <Image src={data.mainImage} alt={data.title} width={800} height={600} />
+      <Image
+        src={data.mainImage}
+        alt={data.title}
+        width={800}
+        height={600}
+        className="duration-200 group-hover:scale-105"
+      />
       <div className="glassmorph absolute bottom-0 flex w-full flex-col gap-1 border-none p-4 opacity-0 duration-200 group-hover:opacity-100 md:group-first:p-8">
         <p className="font-semibold md:group-first:text-xl">{data.title}</p>
         <p className="text-xs md:group-first:text-base">{data.tagline}</p>
