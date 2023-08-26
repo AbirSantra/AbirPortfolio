aa6276d5dec780eefb375e24bf8e841ee087810a
Abir Santra <santraabir8@gmail.com>
2023-08-26 20:12:34 +0530

    changed title and subtitle

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -7,8 +7,11 @@ const Projects = async () => {
   const projectDocs = await getAllProjects();
   return (
     <div className="section">
-      <div className="flex w-full flex-col gap-8">
-        <SectionHeader title="Projects" subtitle="I'm proud of these" />
+      <div className="flex w-full flex-col gap-16">
+        <SectionHeader
+          title="All Projects"
+          subtitle="I learn better by doing"
+        />
         <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-2">
           {projectDocs.map((project) => (
             <ProjectCard data={project} key={project._id} />
