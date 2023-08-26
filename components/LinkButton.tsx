c531ecffa15f864e8744988446cc4d29c536ecfd
Abir Santra <santraabir8@gmail.com>
2023-08-26 18:54:03 +0530

    refactored to add variants

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -5,17 +5,20 @@ type Props = {
   href: string;
   children: React.ReactNode;
   variant: "small" | "large";
+  hover?: boolean;
 };
 
-export const LinkButton = ({ children, href, variant }: Props) => {
+export const LinkButton = ({ children, href, variant, hover }: Props) => {
   return (
     <Link
       href={href}
-      className={`gradient-bg flex w-fit items-center justify-center gap-2  font-medium text-white duration-200 hover:scale-105 ${
+      className={`gradient-bg flex w-fit items-center justify-center   font-medium text-white duration-200 ${
+        hover && "hover:scale-105"
+      } ${
         variant === "small"
-          ? "rounded-lg px-3 py-2 text-xs"
-          : "rounded-xl px-6 py-4 text-sm"
-      }}`}
+          ? "gap-1 rounded-lg px-3 py-2 text-xs"
+          : "gap-2 rounded-xl px-6 py-4 text-sm"
+      }`}
     >
       {children}
     </Link>
