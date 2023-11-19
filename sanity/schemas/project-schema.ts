47ef67b12d25f604bfdeaace0effad3227fe6d06
Abir Santra <santraabir8@gmail.com>
2023-11-19 10:02:57 +0530

    added orderRank field to project schema

--- sanity/schemas/project-schema.ts
+++ sanity/schemas/project-schema.ts
@@ -1,65 +1,72 @@
+import {
+  orderRankField,
+  orderRankOrdering,
+} from "@sanity/orderable-document-list";
+
 const project = {
-  name: 'project',
-  title: 'Projects',
-  type: 'document',
+  name: "project",
+  title: "Projects",
+  type: "document",
+  orderings: [orderRankOrdering],
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    orderRankField({ type: "project" }),
+    {
+      name: "tagline",
+      title: "Tagline",
+      type: "string",
     },
     {
-      name: 'tagline',
-      title: 'Tagline',
-      type: 'string',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title" },
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {source: 'title'},
-    },
-    {
-      name: 'mainImage',
-      title: 'Main Image',
-      type: 'image',
+      name: "mainImage",
+      title: "Main Image",
+      type: "image",
       options: { hotspot: true },
       fields: [
         {
-          name: 'alt',
-          title: 'Alt text',
-          type: 'string',
-        }
-      ]
+          name: "alt",
+          title: "Alt text",
+          type: "string",
+        },
+      ],
     },
     {
-      name: 'description',
-      title: 'Description',
-      type: 'array',
-      of: [{type: 'block'}]
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
     },
     {
-      name: 'githubLink',
-      title: 'Github Link',
-      type: 'url',
+      name: "githubLink",
+      title: "Github Link",
+      type: "url",
     },
     {
-      name: 'projectLink',
-      title: 'Project Link',
-      type: 'url',
+      name: "projectLink",
+      title: "Project Link",
+      type: "url",
     },
     {
-      name: 'tools',
-      title: 'Tools',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'tool'}}]
+      name: "tools",
+      title: "Tools",
+      type: "array",
+      of: [{ type: "reference", to: { type: "tool" } }],
     },
     {
-      name: 'isFeatured',
-      title: 'Set Featured?',
-      type: 'boolean',
-    }
-  ]
-}
+      name: "isFeatured",
+      title: "Set Featured?",
+      type: "boolean",
+    },
+  ],
+};
 
 export default project;
