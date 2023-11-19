83c41a438ab7ad3e4339ccd551900d769093d2a8
Abir Santra <santraabir8@gmail.com>
2023-11-19 09:53:34 +0530

    added rankOrder field to tools schema

--- sanity/schemas/tool-schema.ts
+++ sanity/schemas/tool-schema.ts
@@ -1,30 +1,37 @@
+import {
+  orderRankField,
+  orderRankOrdering,
+} from "@sanity/orderable-document-list";
+
 const tool = {
-  name: 'tool',
-  title: 'Tools',
-  type: 'document',
+  name: "tool",
+  title: "Tools",
+  type: "document",
+  orderings: [orderRankOrdering],
   fields: [
     {
-      name: 'name',
-      title: 'Name',
-      type: 'string'
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    orderRankField({ type: "tool" }),
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+      },
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "icon",
+      title: "Icon",
+      type: "image",
       options: {
-        source: 'name'
-      }
+        hotspot: true,
+      },
     },
-    {
-      name: 'icon',
-      title: 'Icon',
-      type: 'image',
-      options: {
-        hotspot: true
-      }
-    }
-  ]
-}
+  ],
+};
 
 export default tool;
