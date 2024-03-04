816a007ff5257a810be0cd2edb4821a75e1ba898
Abir Santra <santraabir8@gmail.com>
2024-03-04 14:57:15 +0530

    feat: add grid background

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -15,7 +15,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className="scroll-smooth bg-neutral-50 font-Poppins  text-neutral-950 antialiased transition-all duration-200 ease-linear dark:bg-neutral-900 dark:text-white">
+      <body className="scroll-smooth bg-neutral-50 font-Poppins  text-neutral-950 antialiased transition-all duration-200 ease-linear  dark:bg-neutral-900 dark:text-white ">
+        <div className="fixed -z-10 h-full w-full bg-[radial-gradient(#cbcbcba6_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_30%,transparent_100%)] dark:bg-[radial-gradient(#353535a6_1px,transparent_1px)]"></div>
         <header className="sticky top-0 z-50">
           <Navbar />
         </header>
