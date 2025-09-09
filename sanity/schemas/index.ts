1d0748d7d35c0ff92afcde0a7a4ccbc95d31c5e9
Abir Santra <santraabir8@gmail.com>
2025-09-09 18:15:28 +0530

    Clean slate - Remove all project files and configuration
    Deleted all source code, configuration, assets, and documentation files from the repository, including app, components, sections, types, sanity, public assets, and config files. This effectively cleans the repository of all project content.

--- sanity/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import education from "./education-schema";
-import experience from "./experience-schema";
-import project from "./project-schema";
-import tool from "./tool-schema";
-
-const schemas = [project, tool, experience, education];
-
-export default schemas;
--- types/Tool-type.ts
+++
@@ -1,6 +0,0 @@
-export type Tool = {
-  _id: string;
-  name: string;
-  slug: string;
-  icon: string;
-}
